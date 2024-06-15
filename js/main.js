17a8cbaeeb77006fa00a81a7f5ff11c09f7ec9ef
Mikolajwd <mikolaj.wd0821@gmail.com>
2024-06-15 11:24:52 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-window.load = () => {
-	if ('ServiceWorker' in navigator) {
+window.onload = () => {
+	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker
 			.register('./sw.js')
 			.then((reg) => {
